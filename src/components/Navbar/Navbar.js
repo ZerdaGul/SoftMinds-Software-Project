bc416a080accef0cf968f08dcf421339e524b796
Anhelina <angelibond16@gmail.com>
2024-10-20 01:19:41 +0300

    favicons added, structure adjusted

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,17 @@
 import React from 'react';
+
+import earth from '../../assets/earth.svg'
+import user from '../../assets/user.svg'
+import logo from '../../assets/logo.png'
 import './Navbar.css';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="navbar-logo">
-                <a href="/"><img src="/components/logo.png" style={{height:"90px", width: "auto"}}/> </a>
+                <a href="/">
+                    <img src={logo} alt='logo' style={{height:"90px", width: "auto"}}/> 
+                </a>
             </div>
             <ul className="navbar-links">
                 <li><a href="/aboutUs">About Us</a></li>
@@ -14,8 +20,8 @@ const Navbar = () => {
                 <li><a href="/solutions">Solutions</a></li>
                 <li><a href="/consultancy">Consultancy</a></li>
                 <li><a href="/contactUs">Contact Us</a></li>
-                <li><a href="/"> <img src="/components/earth.png" style={{height:"20px", width: "auto"}}/> </a></li>
-                <li><a href="/"> <img src="/components/user.png" style={{height:"40px", width: "auto"}}/> </a></li>
+                <li><a href="/"> <img src={earth} alt='languages' style={{height:"20px", width: "auto"}}/> </a></li>
+                <li><a href="/"> <img src={user} alt='user' style={{height:"40px", width: "auto"}}/> </a></li>
                 <li><a href="/login">Log In</a></li>
             </ul>
             <div className="navbar-toggle">
