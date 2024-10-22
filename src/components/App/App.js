0e8ea125284c0b327abda1d9ac8e20d0d8381f3b
Anhelina <angelibond16@gmail.com>
2024-10-22 18:11:00 +0300

    general styles added, signin form in progress

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
-import Navbar from '../Navbar/Navbar';
+import Navbar from '../navbar/Navbar';
+import SignInForm from '../forms/SignInForm';
 
 function App() {
   return (
     <>
       <Navbar />
-      
+      <SignInForm/>
     </>
   );
 }
