b6f312f243c64b0e385a3935e5e86b4df28b3ee1
MehmetEmirAlbayrak <emirlela240@gmail.com>
2024-10-29 21:56:40 +0300

    commit3

--- api/Controller/updateController.cs
+++ api/Controller/updateController.cs
@@ -54,6 +54,7 @@ public class UpdateController : ControllerBase
             return BadRequest("E-posta adresi doğrulanamadı.");
         }
 
+
         // Only update if Password is provided
         if (!string.IsNullOrEmpty(model.CurrentPassword))
         {
@@ -70,34 +71,7 @@ public class UpdateController : ControllerBase
                 return BadRequest("Geçersiz mevcut şifre.");
             }
         }
-        if (!string.IsNullOrEmpty(model.Password))
-        {
-
-            var passwordPattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$";
-            if (!Regex.IsMatch(model.Password, passwordPattern))
-            {
-                return BadRequest("Şifre en az 8 karakterden oluşmalı, en az bir büyük harf, bir küçük harf ve bir sayı içermelidir.");
-            }
-
-            // Generate a new salt for hashing
-            byte[] salt = new byte[128 / 8];
-            using (var rng = RandomNumberGenerator.Create())
-            {
-                rng.GetBytes(salt);
-            }
-
-            // Hash the password using the new salt
-             hashedPassword = Convert.ToBase64String(KeyDerivation.Pbkdf2(
-                password: model.Password,
-                salt: salt,
-                prf: KeyDerivationPrf.HMACSHA256,
-                iterationCount: 100000,
-                numBytesRequested: 256 / 8));
-
-            // Store the hashed password and the salt
-            existingUser.Password_Hash = hashedPassword;
-            existingUser.Password_Salt = Convert.ToBase64String(salt);
-        }
+        
         if (!string.IsNullOrEmpty(model.Email))
         {
             
@@ -124,13 +98,85 @@ public class UpdateController : ControllerBase
             existingUser.Country = model.Country;
         }
 
+
         _context.Users.Update(existingUser);
         await _context.SaveChangesAsync();
 
         return Ok("Kullanıcı başarıyla güncellendi.");
     }
 
+    // POST /api/update/reset-password
+    [HttpPost("reset-password")]
+    public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordModel model)
+    {
+        
+        if (model == null || string.IsNullOrEmpty(model.CurrentEmail))
+        {
+            return BadRequest("E-posta alanı gereklidir.");
+        }
 
+        // Email validation
+        var emailValidator = new EmailAddressAttribute();
+        if (!emailValidator.IsValid(model.CurrentEmail))
+        {
+            return BadRequest("Geçersiz email adresi.");
+        }
+
+        var existingUser = await _context.Users.FirstOrDefaultAsync(u => u.Email == model.CurrentEmail);
+        if (existingUser == null)
+        {
+            return BadRequest("Bu e-posta adresi ile kayıtlı bir kullanıcı bulunamadı.");
+        }
+
+        if(existingUser.Email != model.CurrentEmail)
+        {
+            return BadRequest("E-posta adresi doğrulanamadı.");
+        }
+
+        // Validate the current password
+        var hashedPassword = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: model.CurrentPassword,
+            salt: Convert.FromBase64String(existingUser.Password_Salt),
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 100000,
+            numBytesRequested: 256 / 8));
+
+        if (hashedPassword != existingUser.Password_Hash)
+        {
+            return BadRequest("Geçersiz mevcut şifre.");
+        }
+
+        // Validate the new password
+        var passwordPattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$";
+        if (!Regex.IsMatch(model.Password, passwordPattern))
+        {
+            return BadRequest("Şifre en az 8 karakterden oluşmalı, en az bir büyük harf, bir küçük harf ve bir sayı içermelidir.");
+        }
+
+        // Generate a new salt for hashing
+        byte[] salt = new byte[128 / 8];
+        using (var rng = RandomNumberGenerator.Create())
+        {
+            rng.GetBytes(salt);
+        }
+
+        // Hash the password using the new salt
+        hashedPassword = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: model.Password,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 100000,
+            numBytesRequested: 256 / 8));
+
+        // Store the hashed password and the salt
+        existingUser.Password_Hash = hashedPassword;
+        existingUser.Password_Salt = Convert.ToBase64String(salt);
+
+        _context.Users.Update(existingUser);
+        await _context.SaveChangesAsync();
+
+        return Ok("Şifre başarıyla güncellendi.");
 
     }
+}
 
