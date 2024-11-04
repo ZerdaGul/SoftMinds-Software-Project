592171665681836c5fcb3e41d97b1b1f147fc408
Semih Sarıkoca <ssarikoca7@gmail.com>
2024-11-04 19:17:42 +0300

    indentation

--- api/Models/Comments.cs
+++ api/Models/Comments.cs
@@ -5,13 +5,13 @@ namespace api.Models;
 public class Comments
 {
     public int Id { get; set; }
-    public int User_Id { get; set; }
-    public int  Product_Id { get; set; }
-    public required string Text { get; set; }=null!;
+    public int UserId { get; set; }
+    public int ProductId { get; set; }
+    public required string Text { get; set; } = null!;
     public DateTime Order_Date { get; set; }
 
-    public required Users User { get; set; }= null!;
-    public required Products Product { get; set; }= null!;
+    public required Users User { get; set; } = null!;
+    public required Products Product { get; set; } = null!;
 
 }
 
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbc16b0257cf9d3d3901f8299ba5d04798bde889")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+968eb2c7d577f359660005a2a080624902f660c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
