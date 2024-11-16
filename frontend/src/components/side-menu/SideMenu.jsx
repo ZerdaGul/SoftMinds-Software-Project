71fa89f2b081c5cceca424078a7e4b2559635044
anhelina-bond <124795789+anhelina-bond@users.noreply.github.com>
2024-11-16 15:29:29 +0300

    request indicator ui

--- frontend/src/components/side-menu/SideMenu.jsx
+++ frontend/src/components/side-menu/SideMenu.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 // import cart from '../../assets/icons/profile/cart.svg';
@@ -8,6 +8,18 @@ import user from '../../assets/icons/profile/user.svg';
 import "./sideMenu.scss"
 
 const SideMenu = ({main_menu}) => {
+    const [newRequest, setNewRequest] = useState(0);
+    useEffect(()=> {
+        main_menu.forEach(({text}) => {
+            if(text === 'Requests') {
+                //load requests
+                //temporarly:
+                setNewRequest(2)
+            }
+        })
+    },[])
+
+    
   return (
     <aside className='side-menu'>
         <div className="side-menu__title">Main menu</div>
@@ -15,25 +27,16 @@ const SideMenu = ({main_menu}) => {
             {main_menu.map(({path, icon, alt, text}) => {
                 return(
                     <li>
-                        <NavLink className='side-menu__link' to={path}
+                        <NavLink key={text} className='side-menu__link' to={path}
                             style={({isActive}) => ({backgroundColor: isActive ? '#F5F2F2' : 'inherit'})}>  
                             <img src={icon} alt={alt}></img>
-                            {text}</NavLink>
+                            {text}
+                            {newRequest && text==="Requests" && <div className="indicator">{newRequest}</div>}</NavLink>
+
+                        
                     </li>
                 )
             })}
-            {/* <li>
-                <NavLink className='side-menu__link' to='/profile/dashboard'
-                    style={({isActive}) => ({backgroundColor: isActive ? '#F5F2F2' : 'inherit'})}>   
-                    <img src={dashboard} alt='dashboard'></img>
-                    Dashboard</NavLink>
-            </li>
-            <li>
-                <NavLink className='side-menu__link' to="/profile/orders"
-                    style={({isActive}) => ({backgroundColor: isActive ? '#F5F2F2' : 'inherit'})}>  
-                    <img src={orders} alt='orders'></img>
-                    Orders</NavLink>
-            </li> */}
         </ul>
         <div className="side-menu__title">Account</div>
         <ul className="side-menu__link-wrapper">
