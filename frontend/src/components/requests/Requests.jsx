6c84929be8eef80a113fd35f839617dede86d882
anhelina-bond <124795789+anhelina-bond@users.noreply.github.com>
2024-11-16 16:11:01 +0300

    button styles updated

--- frontend/src/components/requests/Requests.jsx
+++ frontend/src/components/requests/Requests.jsx
@@ -3,24 +3,33 @@ import React from 'react'
 import './requests.scss';
 const orders=[
     {product: 'Eko Cam 1250',
-        amount: 3},
+        amount: 3,
+        price: 500
+    },
         {product: 'Eko Cam 1250',
-        amount: 1},
+        amount: 1,
+        price:1243
+    },
         {product: 'Eko Cam 1250',
-        amount: 150},
+        amount: 150,
+        price: 555
+    },
         {product: 'Eko Cam 1250',
-        amount: 16},
+        amount: 16,
+        price: 8945
+    },
         
 ]
 const Requests = () => {
   return (
     <section className='requests'>
         <ul className="requests__products">
-                {orders.map(({product, amount}, i) => {
+                {orders.map(({product, amount, price}, i) => {
                     return(
                         <li key={i} className="requests__product">             
                             <div className="requests__product-name">{product}</div>
                             <div className="requests__product-amount">{amount}</div>
+                            <div className="requests__product-price">${price}</div>
                             <div className="requests__buttons">
                                 <button className="button button__small button__small-white">Reject</button>
                                 <button className="button button__small ">Accept</button>
