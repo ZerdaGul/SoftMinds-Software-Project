4244138ab43704cf7c43e040023528b45eed0b9e
anhelina-bond <124795789+anhelina-bond@users.noreply.github.com>
2024-12-09 01:41:54 +0300

    ready to publish

--- api/Program.cs
+++ api/Program.cs
@@ -46,8 +46,9 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy", builder =>
-    {
-        builder.WithOrigins("http://localhost:3000") // React uygulamasının adresi
+    {   
+        builder.WithOrigins("https://ekoinv.com", "https://api.ekoinv.com") // React uygulamasının adresi
+        // builder.WithOrigins("http://localhost:3000") 
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
